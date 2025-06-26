769561dacf638c154c73aeb310e89ab2d63661a2
user <malispierre007@gmail.com>
2025-06-26 15:14:28 +0200

    postgres

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -87,15 +87,15 @@ WSGI_APPLICATION = 'cinema.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'cinema',
-        # 'USER': 'user',
-        # 'PASSWORD': 'user',
-        # 'HOST': 'database',#'localhost',#'database',
-        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cinema',
+        'USER': 'user',
+        'PASSWORD': 'user',
+        'HOST': 'database',#'localhost',#'database',
+        'PORT': '5432',
         
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
